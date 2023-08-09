c8a1a1854598a6fbf426a533efca9dbb007a01c3
jeanfes <jeanescobar7@hotmail.com>
2023-08-09 14:59:49 -0500

    se agrego menu para mobile y se corrijio algunos contenedores en version mobile

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const iconMenu = document.getElementById("icon_menu");
+    const menuMobile = document.getElementById("menu_mobile");
+    const iconCloseMenu = document.getElementById("close_menu");
+    const htmlElement = document.querySelector("html");
+    iconMenu.addEventListener("click", openMenu);
+    iconCloseMenu.addEventListener("click", closeMenu);
+
+    function openMenu() {
+        menuMobile.classList.add("active");
+        htmlElement.classList.add("lock-scroll");
+    }
+
+    function closeMenu() {
+        menuMobile.classList.remove("active");
+        htmlElement.classList.remove("lock-scroll");
+    }
+});
