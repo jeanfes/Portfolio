0a63c869312acba045a2ffbe0d03f1bec43af1b1
Jean Escobar <jeanescobar7@hotmail.com>
2023-08-20 20:02:20 -0500

    cambio a la frecuencia en la que aparece el loading

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,26 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function() {
     const loading = document.getElementById("loading");
-    const htmlElement = document.querySelector("html"); // Cambia el selector según tu necesidad
-
-    // Verificar si la página ya se ha cargado antes
-    const pageLoadedBefore = localStorage.getItem("pageLoadedBefore");
-
-    if (!pageLoadedBefore) {
-        // Mostrar el loading y bloquear el desplazamiento
-        htmlElement.classList.add("lock-scroll");
-
-        setTimeout(function () {
-            if (loading) {
-                loading.style.display = "none";
-                // Eliminar la clase "lock-scroll" y almacenar en el almacenamiento local
-                htmlElement.classList.remove("lock-scroll");
-                localStorage.setItem("pageLoadedBefore", "true");
-            }
-        }, 3000); // 5000 milisegundos = 5 segundos
-    } else {
-        // Si la página ya se ha cargado antes, ocultar el loading
-        if (loading) {
-            loading.style.display = "none";
-        }
+  
+    if (loading) {
+      loading.style.display = "block"; // Mostrar el loading
+  
+      setTimeout(function() {
+        loading.style.display = "none"; // Ocultar el loading después de 3 segundos
+      }, 2000); // 3000 milisegundos = 3 segundos
     }
-});
+  };
+  
