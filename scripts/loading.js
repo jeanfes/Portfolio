959abfa99189549038c6246143923d7e18eef121
Jean Escobar <jeanescobar7@hotmail.com>
2023-08-21 11:54:34 -0500

    se ajusto el scroll mientras aparece la pagina de carga

--- scripts/loading.js
+++ scripts/loading.js
@@ -7,7 +7,7 @@ window.onload = function() {
       setTimeout(function() {
         htmlElement.classList.remove("lock-scroll");
         loading.style.display = "none"; // Ocultar el loading después de 3 segundos
-      }, 2000); // 3000 milisegundos = 3 segundos
+      }, 2000); // 2000 milisegundos
     }
   };
   
