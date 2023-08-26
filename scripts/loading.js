f8226b35fa765b9f2ea32f7ce2dd1fbafbadd954
jeanfes <jeanescobar7@hotmail.com>
2023-08-26 15:13:27 -0500

    ajuste el bloqueo del scroll en la pantalla del loading para que aparezca antes de la animacion de carga

--- scripts/loading.js
+++ scripts/loading.js
@@ -2,8 +2,8 @@ window.onload = function() {
     const loading = document.getElementById("loading");
   
     if (loading) {
-      loading.style.display = "block"; // Mostrar el loading
       htmlElement.classList.add("lock-scroll");
+      loading.style.display = "block"; // Mostrar el loading
       setTimeout(function() {
         htmlElement.classList.remove("lock-scroll");
         loading.style.display = "none"; // Ocultar el loading después de 3 segundos
